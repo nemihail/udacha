ba7e9fb9926668575f3c2e32926b8dae530a38a5
Shulepov Michael <nemihail02@gmail.com>
2025-11-28 10:02:20 +0700

    some pinmodes

---
+++ udacha/udacha.ino
@@ -0,0 +1,20 @@
+
+#define light_relay_pin 2
+#define kettle_relay_pin 4  // пины реле
+#define fan_relay_pin 7
+#define humid_relay_pin 8
+
+#define motor_pin 3
+
+#define hall_light_pin A0
+#define room_light_pin A1
+
+void setup() {
+  pinMode()
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
