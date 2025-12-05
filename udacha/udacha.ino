87300b32924c53baca0947fe72cb300ab3a54c6b
Shulepov Michael <nemihail02@gmail.com>
2025-12-05 09:58:49 +0700

    some new funcs
    temp and humid processing, almost relay

--- udacha/udacha.ino
+++ udacha/udacha.ino
@@ -1,20 +1,43 @@
 
+#include "DHT.h"
+#include <DHT.h>
+
 #define light_relay_pin 2
-#define kettle_relay_pin 4  // пины реле
+#define kettle_relay_pin 4  // relay pins
 #define fan_relay_pin 7
 #define humid_relay_pin 8
 
-#define motor_pin 3
+#define hall_move_pin A0
+#define room_move_pin A1
 
-#define hall_light_pin A0
-#define room_light_pin A1
+#define temphum_pin 3
+
+DHT mydht(temphum_pin, DHT11); 
+
+bool temp, humid; // need to make situation better? or not?
 
 void setup() {
-  pinMode()
+  pinMode(light_relay_pin, OUTPUT);
+  pinMode(kettle_relay_pin, OUTPUT);
+  pinMode(fan_relay_pin, OUTPUT);
+  pinMode(humid_relay_pin, OUTPUT);
 
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
 
+void temphum_proc(){
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+  if (isnan(h) || isnan(t)) {  
+    Serial.println("Ошибка считывания");
+  }
+  if(t > 26){temp = 1;}else{temp = 0;}
+  if(h < 35){humid = 1}else{humid = 0;}
+}
+
+void relay_proc(bool hall_move, bool live_move, bool temp, bool humid, bool light){
+  if(){}else{}
 }
