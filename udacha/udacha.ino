2e5ea1267ab2b485e6b16098a5b7716d2f66dced
Shulepov Michael <nemihail02@gmail.com>
2025-12-24 09:49:07 +0700

    a lot of working code
    fully completed relay_proc function.
    time processing left, i'll do that
    and of course, many many bugs...

--- udacha/udacha.ino
+++ udacha/udacha.ino
@@ -1,10 +1,8 @@
 #include <TimeLib.h>
 #include <Wire.h>                    
 #include <LiquidCrystal_I2C.h>        
-LiquidCrystal_I2C lcd(0x27,20,4);
 
-#include "DHT.h"
-#include <DHT.h>
+#include "DHT.h" // 
 
 #define light_relay_pin 2
 #define kettle_relay_pin 4  // relay pins
@@ -16,45 +14,58 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 
 #define temphum_pin 3
 
+#define light_pin A2
+
 DHT mydht(temphum_pin, DHT11); 
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 bool temp, humid; // need to make situation better? or not?
-
-short light_limit;
 
 void setup() {
   pinMode(light_relay_pin, OUTPUT);
   pinMode(kettle_relay_pin, OUTPUT);
   pinMode(fan_relay_pin, OUTPUT);
   pinMode(humid_relay_pin, OUTPUT);
+  
   lcd.init();                      
-  lcd.backlight();                  
+  lcd.backlight();  
+                  
   Serial.begin(9600);
+  
   setTime(8, 20, 0, 2419, 12, 2025); // 12 января 2024, 14:30:45
+  
   dht.begin();
 }
 
 void loop() {
-  int h = dht.readHumidity(); 
-  int t = dht.readTemperature();   
+  temphum_proc();
+  relay_proc(digitalRead(hall_move_pin),
+             digitalRead(room_move_pin),
+             temp, humid, digitalRead(light_pin));
+  lcdproc();
+}
+
+void lcdproc(){ 
   lcd.setCursor(0, 0);              
   lcd.print("Temperature: "); 
-  lcd.print(t);               
+  lcd.print(t);     
+            
   lcd.setCursor(0, 1);             
   lcd.print("Humidity: ");
   lcd.print(h);    
-  lcd.setCursor(0, 2);             
-  lcd.print("Time:");
+  lcd.setCursor(0, 2);    
+           
+  lcd.print("Time: ");
   lcd.print(hour()); 
   lcd.print(':');
   lcd.print(minute());
   lcd.print(':');  
-  lcd.print(second());   
+  lcd.print(second());  
 }
 
 void temphum_proc(){
-  t = dht.readTemperature();
-  h = dht.readHumidity();
+  short t = dht.readTemperature();
+  short h = dht.readHumidity();
   if (isnan(h) || isnan(t)) {  
     Serial.println("Ошибка считывания");
   }
@@ -63,15 +74,26 @@ void temphum_proc(){
 }
 
 void relay_proc(bool hall_move, bool live_move, bool temp, bool humid, bool light){
-  if(temp && live_move){digitalWrite(fan_relay_pin, 1);}else{digitalWrite(fan_relay_pin, 0);} // вентилятор если жарко и в комнате кто-то есть
-  if(humid){digitalWrite(humid_relay_pin, 1);}else{digitalWrite(humid_relay_pin, 0);} // увлажнитель в комнате если влажно
-  if(light){digitalWrite(light_relay_pin, 1);}else{digitalWrite(light_relay_pin, 1);} // свет в комнате
+  if(temp && live_move){
+    digitalWrite(fan_relay_pin, 1);
+  }else{
+    digitalWrite(fan_relay_pin, 0);
+  }
+  if(humid){
+    digitalWrite(humid_relay_pin, 1);
+  }else{
+    digitalWrite(humid_relay_pin, 0);
+  }
+  if(light && live_move){
+    digitalWrite(light_relay_pin, 1);
+  }else{
+    digitalWrite(light_relay_pin, 0);
+  }
   if(hall_move){
     digitalWrite(kettle_relay_pin, 1);
-    delay(60000); // one minute delay to boil the water
+    delay(60000);
     digitalWrite(kettle_relay_pin, 0);
-  }else{digitalWrite(kettle_relay_pin, 0);}
+  }else{
+    digitalWrite(kettle_relay_pin, 0);
+  }
 }
-
-
-
