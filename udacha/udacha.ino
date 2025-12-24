7fa6200513390abd51c0bc692cb72d430948ed52
tereshonkov-sys <ivanteresonkov56@gmail.com>
2025-12-24 08:21:02 +0700

    Update udacha.ino

--- udacha/udacha.ino
+++ udacha/udacha.ino
@@ -1,3 +1,7 @@
+#include <TimeLib.h>
+#include <Wire.h>                    
+#include <LiquidCrystal_I2C.h>        
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 #include "DHT.h"
 #include <DHT.h>
@@ -23,11 +27,29 @@ void setup() {
   pinMode(kettle_relay_pin, OUTPUT);
   pinMode(fan_relay_pin, OUTPUT);
   pinMode(humid_relay_pin, OUTPUT);
-
+  lcd.init();                      
+  lcd.backlight();                  
+  Serial.begin(9600);
+  setTime(8, 20, 0, 2419, 12, 2025); // 12 января 2024, 14:30:45
   dht.begin();
 }
 
 void loop() {
+  int h = dht.readHumidity(); 
+  int t = dht.readTemperature();   
+  lcd.setCursor(0, 0);              
+  lcd.print("Temperature: "); 
+  lcd.print(t);               
+  lcd.setCursor(0, 1);             
+  lcd.print("Humidity: ");
+  lcd.print(h);    
+  lcd.setCursor(0, 2);             
+  lcd.print("Time:");
+  lcd.print(hour()); 
+  lcd.print(':');
+  lcd.print(minute());
+  lcd.print(':');  
+  lcd.print(second());   
 }
 
 void temphum_proc(){
@@ -46,4 +68,5 @@ void relay_proc(bool hall_move, bool live_move, bool temp, bool humid, bool ligh
   if(light){digitalWrite(light_relay_pin, 1);}else{digitalWrite(light_relay_pin, 1);} // свет в комнате
   // остался только чайник. еще надо написать меню для лсд, обработку времени, и (несложно) света.
 }
+
 
